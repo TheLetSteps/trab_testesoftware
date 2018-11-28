a1d86f5b15388e701b235ad5476f66539379ec09
soluclea <lucas.alsilva01@gmail.com>
2018-11-28 19:46:31 -0400

    Update teste login

--- src/main/java/TestWEB.java
+++ src/main/java/TestWEB.java
@@ -6,17 +6,17 @@ import org.junit.Test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestWEB {
 		
 	WebDriver driver;
-	private String url = "localhost:8080/TrabalhoTesteSoftware";
+	private String url = "http://localhost:8080/TrabalhoTesteSoftware/index.jsp";
 
-	private String number1 = "15";
-	private String number2 = "10";
-	private String resultado = "25";
+	private String login_email = "lucas.alsilva01@gmail.com";
+	private String login_pass = "123456";
 	
     @Before
     public void start() throws Throwable {
@@ -33,8 +33,7 @@ public class TestWEB {
         driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
  
         Thread.sleep(1000);
- 
-        if (driver.getPageSource().contains("Saraiva")) {
+        if (driver.getPageSource().contains("linkedin")) {
             System.out.println("Pass");
         } else {
             System.out.println("Fail");
@@ -43,19 +42,22 @@ public class TestWEB {
     
     @Test
 	public void test() {
-    	/*
-    	driver.findElement(By.id("number1")).sendKeys(number1);
+    	System.out.println("Testando...");
+    	
+    	driver.findElement(By.id("login-email")).sendKeys(login_email);
 		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 
-		driver.findElement(By.id("number2")).sendKeys(number2);
+		driver.findElement(By.id("login-password")).sendKeys(login_pass);
 		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-		
-		driver.findElement(By.id("calcular")).click();
+//		
+		driver.findElement(By.id("login-submit")).click();
 		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-
-		resultado = driver.findElement(By.id("resultado")).getAttribute("value");
-		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-		assertEquals(resultado, "25");
-		*/
+		String current_url = driver.getCurrentUrl();
+		boolean login_sucess= !current_url.contains("unexpected_error");
+		System.out.println(login_sucess);
+		assertEquals(login_sucess, true);
+		if(login_sucess) {
+			System.out.println("Login efetuado com sucesso!");
+		}
 	}	
 }
